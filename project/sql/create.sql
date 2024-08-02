8da9e6067e37410b9c6168d17bd2a669af162232
XZQshiyu <2313392804@qq.com>
2024-08-02 17:03:45 +0800

    fix some bugs in create.sql

--- project/sql/create.sql
+++ project/sql/create.sql
@@ -4,53 +4,55 @@ create database what_can_I_eat;
 use what_can_I_eat;
 
 drop table if exists user;
+drop table if exists campus;
+drop table if exists canteen;
+drop table if exists food_window;
+drop table if exists dish;
+drop table if exists post;
+
 create table user
-{
+(
     user_name varchar(50) primary key,
     uid integer not null,
     introduction varchar(200),
     head_portrait VARCHAR(255)
-}
+);
 
-drop table if exists campus;
 create table campus
-{
+(
     campus_name varchar(30) primary key
-};
+);
 
-drop table if exists canteen;
 create table canteen
-{
+(
     canteen_name varchar(30) primary key,
     location varchar(50) not null
-};
+);
 
-drop table if exists window;
-create table window
-{
+create table food_window
+(
     window_name varchar(30) primary key,
     description varchar(100) not null
-};
+);
 
-drop table if exists dish;
 create table dish
-{
-    dish_name varchar(30) primary key,
+(
+    dish_name varchar(30) not null,
     context varchar(1000) not null,
     image VARCHAR(255),
-    promulgator varchar(30) primary key,
+    promulgator varchar(30) not null,
     publish_time date,
     price float not null,
-    place varchar(50) primary key,
+    place varchar(50) not null,
     like_number integer not null,
     trample_number integer not null,
     collect_number integer not null,
+    primary key (dish_name, promulgator, place),
     foreign key (promulgator) references user(user_name) ON DELETE CASCADE
-};
+);
 
-drop table if exists post;
 create table post
-{
+(
     promulgator varchar(30) primary key,
     context varchar(1000) not null,
     image VARCHAR(255),
@@ -63,4 +65,4 @@ create table post
     tag_3 varchar(30),
     tag_4 varchar(30),
     tag_5 varchar(30)
-}
+);
