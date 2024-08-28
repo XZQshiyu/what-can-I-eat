22ef6684afedc0d32525158e883ca23a82dca06c
Browserandcreator <13918234644@163.com>
2024-08-28 23:12:46 +0800

    add insert/fix create

--- sql/create.sql
+++ sql/create.sql
@@ -16,7 +16,6 @@ create table user
 (
     user_id varchar(18) primary key,
     user_name varchar(30) not null,
-    uid varchar(18) primary key,
     introduction varchar(200),
     head_portrait VARCHAR(255)
 );
@@ -24,26 +23,26 @@ create table user
 create table campus
 (
     campus_id varchar(18) primary key,
-    campus_name varchar(30) primary key
+    campus_name varchar(30) not null
 );
 
 -- 这个canteen的location应该是 campus_name，因此需要一个外键约束，这样才能体现canteen和campus的关系
 create table canteen
 (
     canteen_id varchar(18) primary key,
-    canteen_name varchar(30) primary key,
+    canteen_name varchar(30) not null,
     canteen_location varchar(30) not null,
-    foreign key (canteen_location) references campus(campus_name) ON DELETE CASCADE
+    foreign key (canteen_location) references campus(campus_id) ON DELETE CASCADE
 );
 
 -- 原先的window是真的关键字，所以我改成了 food_window, 同时我觉得这个表应该有一个外键约束，指向canteen
 create table food_window
 (
     window_id varchar(18) primary key,
-    window_name varchar(30) primary key,
+    window_name varchar(30) not null,
     canteen_name varchar(30) not null,
     window_description varchar(100) not null,
-    foreign key (canteen_name) references canteen(canteen_name) ON DELETE CASCADE
+    foreign key (canteen_name) references canteen(canteen_id) ON DELETE CASCADE
 );
 
 -- dish的主键约束修改了
@@ -52,20 +51,19 @@ create table food_window
 -- 如果要写外键约束，这两个变量类型应该相同，即varchar长度一致
 create table dish
 (
-    dish_id varchar(18) primary key,
+    dish_id varchar(18) not null,
     dish_name varchar(30) not null,
     context varchar(255) not null,
     image VARCHAR(255),
-    uploader_uid varchar(18) not null,
+    uploader_id varchar(18) not null,
     publish_time date,
     price float not null,
     dish_location varchar(30) not null,
     like_number integer not null,
     dislike_number integer not null,
     favorite_number integer not null,
-    foreign key (place) references food_window(window_name) ON DELETE CASCADE,
-    primary key (dish_name, uploader_id, dish_location),
-    foreign key (uploader_uid) references user(uid) ON DELETE CASCADE
+    primary key (dish_id, uploader_id, dish_location),
+    foreign key (uploader_id) references user(user_id) ON DELETE CASCADE
 );
 
 -- 虽然但是，promulgator最好是user的外键，即用user_id来表示，可以通过关联查找来定位user的name
@@ -85,5 +83,5 @@ create table post
     tag_3 varchar(30),
     tag_4 varchar(30),
     tag_5 varchar(30),
-    foreign key (uploader_id) references user(uid) ON DELETE CASCADE
+    foreign key (uploader_id) references user(user_id) ON DELETE CASCADE
 );
