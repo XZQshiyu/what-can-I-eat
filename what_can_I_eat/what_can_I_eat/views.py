4285de5dae860ff265bfa19e6499d83ed77591e7
byhx4657 <zyh0325@mail.ustc.edu.cn>
2024-09-04 19:34:39 +0800

    zyh_commit

--- what_can_I_eat/what_can_I_eat/views.py
+++ what_can_I_eat/what_can_I_eat/views.py
@@ -2,6 +2,13 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib import messages
 from django.db import connection
+from django.urls import reverse
+from django.shortcuts import render, redirect
+from django.views.decorators.csrf import csrf_protect
+from django.http import HttpResponseNotFound
+from django.contrib import messages
+from django.core.files.storage import default_storage
+from django.core.files.base import ContentFile
 
 
 def signin(request):
@@ -180,3 +187,124 @@ def add_window(request):
         return HttpResponse("窗口添加成功")
     return render(request, 'windows/add_window.html')
 #?餐厅id要输入还是用来匹配的？
+
+# zyh 没写完
+# 用户接口：
+
+# 展示所有用户 (需要添加："user/user_management.html"、{"users": results}) （可以自己改）
+def user_management(request):
+    with connection.cursor() as cursor:
+        cursor.callproc('GetAllEntity')
+        results = cursor.fetchall()
+    return render(request, "users/user_management.html", {"users": results})
+
+# 添加用户
+def AddUser(request):
+    if request.method == 'POST':
+        # 获取表单数据
+        user_id = request.POST.get('user_id')
+        user_name = request.POST.get('user_name')
+        introduction = request.POST.get('introduction')
+        haed_portrait = request.POST.get('head_portrait')
+
+        #初始化错误信息
+        errors = {}
+
+        # 检查id，要求为正整数
+        if not user_id:
+            errors['user_id'] = 'id 不能为空'
+        else:
+            try:
+                user_id = int(user_id)
+                if user_id <= 0:
+                    errors['user_id'] = 'id必须是正整数'
+            except ValueError:
+                errors['user_id'] = 'id 必须是正整数'
+        
+        # 检查昵称
+        if not user_name:
+            errors['user_name'] = '昵称不能为空'
+        
+        # 检查简介
+        if not introduction:
+            errors['introduction'] = '简介不能为空'
+        # 检查性别
+        if  not haed_portrait :
+            errors['head_portrait'] = "头像不能为空"
+        
+        # 调用存储过程
+        with connection.cursor() as cursor:
+            err = ''
+            cursor.callproc('AddUser', [user_id, user_name, introduction, haed_portrait, err
+            ])
+            cursor.execute('SELECT @_AddUser_4')
+            err = cursor.fetchone()[0]
+            print(err)
+            if err:
+                errors['database'] = 'err' # 这个database我不知道要不要改
+                return render(request, "users/AddUser.html", {"errors": errors})
+            else:
+                return redirect(reverse("banksystem:client")) # 这个我不知道怎么改
+    return render(request, "users/AddUser.html")
+
+# 更新用户信息
+def UpdateUser(request, user_id): # 这个user_id应该是点击对应的用户之后自动传上去的，但是我不知道怎么做（
+    if request.method == 'POST':
+        user_name = request.POST.get('user_name')
+        introduction = request.POST.get('introduction')
+        head_portrait = request.POST.get('head_portrait')
+
+        errors = {}
+
+        if not errors:
+            with connection.cursor() as cursor:
+                err = ''
+                cursor.callproc('UpdateUser', [user_id, user_name or None, introduction or None, head_portrait or None, err])
+                cursor.execute('SELECT @_UpdateUser_4')
+                err = cursor.fetclone()[0]
+                if err:
+                    errors['datanase'] = err # 这个database同理 我也不知道怎么改……
+
+        if errors:
+            return render(request, "users/UpdateUser.html", {"errors": errors})
+        else:
+            return redirect(reverse("banksystem:client"))
+        
+    return render(request, "users/UpdateUser.html")
+
+# 删除用户信息
+def DeleteUser(request): # 感觉这个user_id应该是点击哪个user，哪个user的user_id就被传进去，不用再输入（？
+    if request.method == 'POST':
+        user_id = request.POST.get('user_id')
+        errors = {}
+
+        if not user_id:
+            errors['user_id'] = 'user_id cannot be empty'
+        else:
+            with connection.cursor() as cursor:
+                err = ''
+                cursor.callproc("DeleteUser", [user_id, err])
+                cursor.execute("SELECT @_DeleteUser_1")
+                err = cursor.fetchone()[0]
+                if err:
+                    errors['database'] = err
+        if errors:
+            return render(request, "users/DeleteUser.html", {"errors": errors})
+        else:
+            return redirect(reverse("banksystem:client"))
+
+    return render(request, "users/DeleteUser.html")
+
+# 通过id和name查询用户
+def search_user(request):
+    results = []
+    if request.method == 'POST':
+        user_name = request.POST.get('user_name', None)
+        user_id = request.POST.get('user_id', None)
+
+        with connection.cursor() as cursor:
+            cursor.callproc('search_user', [user_name, user_id])
+            results = cursor.fetchall()
+    return render(request, "users/search_user.html", {"results": results})
+
+# 
